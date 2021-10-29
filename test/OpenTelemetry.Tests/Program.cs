1b6b3dbe5649abf51caf4e9f86d5bf4725634a0a
Yun-Ting Lin <yunl@microsoft.com>
2021-10-29 00:30:00 -0700

    MoveNext benchmark

---
+++ test/OpenTelemetry.Tests/Program.cs
@@ -0,0 +1,29 @@
+// <copyright file="Program.cs" company="OpenTelemetry Authors">
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+
+using BenchmarkDotNet.Running;
+using static OpenTelemetry.Tests.Trace.BatchTestMoveNextBenchmarks;
+
+namespace OpenTelemetry.Tests
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<MoveNextTests>();
+        }
+    }
+}
