0a494855bb2776a17dde9ef178355911322263f9
Yun-Ting <yunl@microsoft.com>
2023-03-24 13:47:37 -0700

    add test

--- test/OpenTelemetry.Tests/Metrics/AggregatorTest.cs
+++ test/OpenTelemetry.Tests/Metrics/AggregatorTest.cs
@@ -15,6 +15,7 @@
 // </copyright>
 
 using System;
+using System.Threading;
 using Xunit;
 
 namespace OpenTelemetry.Metrics.Tests
@@ -132,6 +133,106 @@ namespace OpenTelemetry.Metrics.Tests
             // There should be no enumeration of BucketCounts and ExplicitBounds for HistogramSumCount
             var enumerator = histogramPoint.GetHistogramBuckets().GetEnumerator();
             Assert.False(enumerator.MoveNext());
+        }
+
+        [Fact]
+        public void MultiThreadedHistogramUpdateAndSnapShotTest()
+        {
+            var boundaries = Array.Empty<double>();
+
+            var argsToThread = new UpdateThreadArguments
+            {
+                MreToBlockUpdateThread = new ManualResetEvent(false),
+                MreToEnsureAllThreadsStart = new ManualResetEvent(false),
+                HistogramPoint = new MetricPoint(this.aggregatorStore, AggregationType.Histogram, null, boundaries, Metric.DefaultExponentialHistogramMaxBuckets),
+            };
+
+            var numberOfThreads = 10;
+            Thread[] t = new Thread[numberOfThreads];
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                t[i] = new Thread(HistogramUpdateThread);
+                t[i].Start(argsToThread);
+            }
+
+            argsToThread.MreToEnsureAllThreadsStart.WaitOne();
+            argsToThread.MreToBlockUpdateThread.Set();
+
+            for (int i = 0; i < numberOfThreads; ++i)
+            {
+                t[i].Join();
+            }
+
+            var histogramPoint = argsToThread.HistogramPoint;
+            histogramPoint.TakeSnapshot(true);
+
+            var sum = histogramPoint.GetHistogramSum();
+            Assert.Equal(400, sum);
+
+            var count = histogramPoint.GetHistogramCount();
+            Assert.Equal(70, count);
+
+            // Reset UpdateThreadArguments arguments
+            argsToThread.MreToEnsureAllThreadsStart.Reset();
+            argsToThread.MreToBlockUpdateThread.Reset();
+            argsToThread.ThreadStartedCount = 0;
+
+            Thread[] t2 = new Thread[numberOfThreads];
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                t2[i] = new Thread(HistogramUpdateThread);
+                t2[i].Start(argsToThread);
+            }
+
+            argsToThread.MreToEnsureAllThreadsStart.WaitOne();
+            argsToThread.MreToBlockUpdateThread.Set();
+
+            for (int i = 0; i < numberOfThreads; ++i)
+            {
+                t2[i].Join();
+            }
+
+            var histogramPoint2 = argsToThread.HistogramPoint;
+            histogramPoint2.TakeSnapshot(true);
+
+            var sum2 = histogramPoint2.GetHistogramSum();
+            Assert.Equal(400, sum2);
+
+            var count2 = histogramPoint2.GetHistogramCount();
+            Assert.Equal(70, count2 - count);
+        }
+
+        private static void HistogramUpdateThread(object obj)
+        {
+            if (obj is not UpdateThreadArguments args)
+            {
+                throw new Exception("invalid args");
+            }
+
+            var mreToEnsureAllThreadsStart = args.MreToEnsureAllThreadsStart;
+
+            if (Interlocked.Increment(ref args.ThreadStartedCount) == 10)
+            {
+                mreToEnsureAllThreadsStart.Set();
+            }
+
+            args.MreToBlockUpdateThread.WaitOne();
+            args.HistogramPoint.Update(-10);
+            args.HistogramPoint.Update(0);
+            args.HistogramPoint.Update(1);
+            args.HistogramPoint.Update(9);
+            args.HistogramPoint.Update(10);
+            args.HistogramPoint.Update(11);
+            args.HistogramPoint.Update(19);
+        }
+
+        private class UpdateThreadArguments
+        {
+            public ManualResetEvent MreToBlockUpdateThread;
+            public ManualResetEvent MreToEnsureAllThreadsStart;
+            public int ThreadStartedCount;
+            public MetricPoint HistogramPoint;
+            public int NumberOfThreads = 10;
         }
     }
 }
